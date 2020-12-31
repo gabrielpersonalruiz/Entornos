febc3e3fea25b30b3ad96786104831ab605604ee
Gabriel Personal <gabrielpersonalruiz@gmail.com>
2020-12-31 20:04:21 +0100

    Version en ingles en distinta rama

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,4 +1,32 @@
 package practicaentornos;
-public class Ejemplo{
+import java.util.Scanner;
+
+public class Navidad07 {
+    static int a = (int) ((Math.random() * 100)+1); 
+    static int b; 
+    static int i; 
+   static Scanner teclado = new Scanner(System.in);
+
+public static void main(String[] args) {
+ 
+
+ System.out.print("Try to get the number that im thinking of. Write a number between 1 and 100: ");
+ b = teclado.nextInt();
+
+do{     
+    if (a > b){ 
+      System.out.println("The number is BIGGER");
+      i++;
+    }else if (a < b){
+      System.out.println("The number is SMALLER");
+      i++;
+    }if(a!=b){
+        System.out.print("Try again: ");
+        b=teclado.nextInt();
+    }
+} while (a!=b);
+System.out.println("Correct, You got it right!");  
+System.out.println("You've made "+i+" tries");
+}
 }
 
