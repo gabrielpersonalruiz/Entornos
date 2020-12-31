faa4488d435d55266bda356a5830ff8e39787b45
Gabriel Personal <gabrielpersonalruiz@gmail.com>
2020-12-31 20:10:19 +0100

    Añadir explicación.

--- Ejemplo.java
+++ Ejemplo.java
@@ -29,4 +29,5 @@ System.out.println("Correcto, ¡Has Acertado!");
 System.out.println("Has hecho "+i+" intentos");
 }
 }
-
+/* Lo que hace este programa es generar un número aleatorio entre 1 y 100 
+ * e intenta que tu lo averigues diciendote cuando lo aciertas, la cantidad de intentos que has hecho */
